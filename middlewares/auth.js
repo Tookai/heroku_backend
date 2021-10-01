a7d8733247f0fd0f7b4b24d28afad057dcdce651
Thibaut <tybo.orcel@gmail.com>
2021-10-01 15:00:42 +0200

    deleted env file

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
   if (token) {
-    jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+    jwt.verify(token, SUPER_SECRET_KEY, (err, user) => {
       if (err) res.status(403).json("Le token est invalide.");
 
       req.user = user;
